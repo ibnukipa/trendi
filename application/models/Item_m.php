4d120fd2cb755716ab2b9444c693d7c863f361ec
Prasetya Gilang <pgn.gilang.1994@gmail.com>
2016-05-11 22:19:35 +0700

    model item

--- application/models/Item_m.php
+++ application/models/Item_m.php
@@ -22,7 +22,7 @@ Class Item_m extends CI_Model{
         $query_ = $this->db->query("INSERT INTO fashion(nama, deskripsi, foto, rating, id_admin, kategori, genre, tanggal) VALUES('$nama', '$deskripsi', '$foto', 0, '$id_admin', '$kategori', '$genre', NOW())");
     }
     
-    public function update($id, $nama, $deskripsi, $foto, $id_admin, $kategori, $genre)
+    public function update($id_fashion, $nama, $jenis, $deskripsi, $foto)
     {
         $query_ = $this->db->query("update fashion set nama = '$nama', deskripsi = '$deskripsi', foto = '$foto', id_admin = '$id_admin', kategori = '$kategori', genre = '$genre' where id = '$id'");
     }
