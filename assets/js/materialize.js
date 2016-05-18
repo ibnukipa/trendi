22bd317f7e3b9b0382be1c1e744f7f5cce850428
relacilia <relaci.alia@gmail.com>
2016-05-18 20:35:22 +0700

    .

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -6854,4 +6854,23 @@ $(document).ready(function(){
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
   });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
+$('#modal1').openModal();
+
+$('#modal1').closeModal();
+
+$('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      ready: function() { alert('Ready'); }, // Callback for Modal open
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
         
